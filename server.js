9b2dc648c4c92da5bc5fa7f1500f53b26699c862
Users <jmwaura@byupathway.edu>
2025-06-02 14:39:23 +0300

    commit

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ app.set("layout", "./layouts/layout"); // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"});
+});
 
 /* ***********************
  * Local Server Information
